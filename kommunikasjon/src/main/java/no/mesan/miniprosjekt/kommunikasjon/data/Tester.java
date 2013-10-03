4295bfb2cf7ed41e81f7469947f8c65627c3c274
Thomas Pettersen <tpettersen@gmail.com>
2013-10-03 18:49:46 +0200

    Added DB-connection

---
+++ kommunikasjon/src/main/java/no/mesan/miniprosjekt/kommunikasjon/data/Tester.java
@@ -0,0 +1,9 @@
+package no.mesan.miniprosjekt.kommunikasjon.data;
+
+
+public class Tester {
+	public static void main(String[] argv) {
+		AviationData aviationData = new AviationData();
+		aviationData.getAviationData();
+	}
+}
