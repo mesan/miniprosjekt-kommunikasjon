f54169e86811cc4bb3f2f53f5937e70621946489
kajas <kajas@mesan.no>
2013-11-07 19:37:03 +0100

    gjennomsnittsmaalinger for thrift

--- kommunikasjon/src/main/java/no/mesan/miniprosjekt/kommunikasjon/data/DbTilkobling.java
+++ kommunikasjon/src/main/java/no/mesan/miniprosjekt/kommunikasjon/data/DbTilkobling.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DbTilkobling {
 
-	public static final String CONNECTION_URL = "jdbc:sqlserver://192.168.21.34:1433;"
+	public static final String CONNECTION_URL = "jdbc:sqlserver://192.168.22.45:1433;"
 			+ "databaseName=heckdole_prod;user=miniprosjekt;password=pass;";
 	
 	public Connection getConnection() {
