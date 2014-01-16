a3bbe00dcd17f50cc109069362c593ac4cb3204b
Thomas Pettersen <tpettersen@gmail.com>
2014-01-16 14:58:06 +0100

    Cleaup

--- kommunikasjon/src/main/java/no/mesan/miniprosjekt/kommunikasjon/avro/AvroServerApp.java
+++ kommunikasjon/src/main/java/no/mesan/miniprosjekt/kommunikasjon/avro/AvroServerApp.java
@@ -27,29 +27,7 @@ import org.apache.avro.ipc.specific.SpecificResponder;
 
 public class AvroServerApp {
 	public static class AvroProtocolImpl implements AvroProtocol {
-
-//		@Override
-//		public AvroAircraft send() throws AvroRemoteException {
-//			AvroAircraft avroAircraft= new AvroAircraft();
-//			//AircraftDao dao = new AircraftDao();
-//			OsDao dao = new OsDao();
-//			Os os = dao.getOss().get(0);
-//	        //List<Aircraft> aircrafts = dao.getAircrafts();
-//	       // Aircraft aircraft = aircrafts.get(0);
-//	        
-//	        
-//	        avroAircraft.setName(os.getName());
-//			System.out.println("length drawing server: " + os.getSystem().length);
-//	        
-//	        //ByteBuffer byteBuffer = ByteBuffer.allocate(aircraft.getDrawing().length + 16);
-//	        //byteBuffer.put(aircraft.getDrawing());
-//	        
-//	        avroAircraft.setDrawing(ByteBuffer.wrap(os.getSystem()));
-//	        
-//			System.out.println("length byteBuffer: " + avroAircraft.getDrawing().array().length);
-//			return avroAircraft;
-//		}
-
+		
 		@Override
 		public AvroAircrafts getAircrafts() throws AvroRemoteException {
 			AircraftDao dao = new AircraftDao();
@@ -103,12 +81,8 @@ public class AvroServerApp {
 	public static void main(String[] args) throws IOException {
 
 		System.out.println("Starting server");
-		// usually this would be another app, but for simplicity
 		startServer();
 
 		System.out.println("Server started");
-
-		// server.close();
-		// System.out.println("Stopped");
 	}
 }
